756dd895bc37ef075bc40b44bdce23bcc226b12b
Jessy Besse <bess0003@hz.nl>
2019-12-10 19:10:45 +0100

    Added a map
    side scrolling to each end while walking

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -2,22 +2,36 @@ export class GameScene extends Phaser.Scene {
 
     private char: any & { body: Phaser.Physics.Arcade.Body };
     private cursorKeys: any;
+    private map: any;
 
 
     constructor() {
         super('gamescene');
-
     }
 
     preload() {
-        //load our images or sounds
+        //load in the map
+        this.load.image('map', './assets/images/map.png');
+
+        //load spritesheet for male character
         this.load.spritesheet('boy', './assets/spritesheets/boy.png', { frameWidth: 64, frameHeight: 64 });
+
+        //load spritesheet for female character
+        //this.load.spritesheet('girl', './assets/spritesheets/girl.png', { frameWidth: 64, frameHeight: 64 });
+
 
     }
 
     create() {
 
-        this.char = this.add.sprite(this.game.canvas.width / 2, 200, "boy", 0)
+        // Add map to the scene
+        this.map = this.add.image(this.game.canvas.width / 2, this.game.canvas.height / 2, "map")
+        this.map.displayWidth = 2500;
+        this.map.displayHeight = this.game.canvas.height;
+        this.physics.add.existing(this.map);
+
+        // Add character to the scene
+        this.char = this.add.sprite(this.game.canvas.width / 2, 485, "boy", 0)
         this.physics.add.existing(this.char);
         this.anims.create({
             key: 'idle',
@@ -38,16 +52,23 @@ export class GameScene extends Phaser.Scene {
         this.cursorKeys = this.input.keyboard.createCursorKeys();
 
         if (this.cursorKeys.right.isDown) {
-            this.char.body.setVelocityX(85); // move left with 85 speed
+            this.char.body.setVelocityX(50); // move left with 85 speed
             this.char.anims.play('walk', true); // plays walking animation
-            this.char.flipX= true; // flip the sprite to the left
+            this.char.flipX = true; // flip the sprite to the left
+
+            this.map.body.setVelocityX(-100); // move the background to the right
+
         } else if (this.cursorKeys.left.isDown) {
-            this.char.body.setVelocityX(-85) // move right with 85 speed
+            this.char.body.setVelocityX(-50) // move right with 85 speed
             this.char.anims.play('walk', true); // plays walking animation
             this.char.flipX = false; // use the original sprite looking to the right
+
+            this.map.body.setVelocityX(117); // move left with 85 speed
         } else {
             this.char.body.setVelocityX(0);
             this.char.anims.play('idle', true); // plays the idle animtaion when no keys are pressed
+
+            this.map.body.setVelocityX(0); // move the background to the left
         }
     }
 }
