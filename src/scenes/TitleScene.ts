17b44d5a46ed67cc08d380e60e4e76ee4ea0272c
Danny Lifino <dannylif@hotmail.com>
2019-12-10 12:13:58 +0100

    next button, updated gitignore to include vscode

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -6,6 +6,8 @@ export class TitleScene extends Phaser.Scene {
             pack: {
                 files: [
                     { type: 'image', key: 'title', url: './assets/images/game-title.png' },
+                    { type: 'image', key: 'next', url: './assets/images/next-button.jpg' },
+
                 ]
             }
         });
@@ -18,8 +20,12 @@ export class TitleScene extends Phaser.Scene {
         music.play();
 
         // Add title image
-        const titleImg = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height / 4, 'title');
-        titleImg.setInteractive();
+        const title = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height / 5, 'title');
+
+        // Add next button image
+        const nextBtn = this.add.image(this.sys.canvas.width / 2, this.sys.canvas.height * 0.9, 'next');
+        nextBtn.setScale(0.2);
+        nextBtn.setInteractive();
 
     }
 }
